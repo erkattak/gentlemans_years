bd1b62813e2cb84f52d4c37acec0c5367bf38995
Erik Straub <erik.straub@oddnetworks.com>
2017-03-16 11:09:27 -0400

    Updates to work as expected

--- lib/gentlemans_years.rb
+++ lib/gentlemans_years.rb
@@ -3,7 +3,8 @@ require "active_support/all"
 
 class Numeric
   def gentlemans_years
-      ActiveSupport::Duration.new((self * (rand() + 1)) * 365.25.days.to_i, [[:years, self]])
+    num = self * (rand() + 1)
+    ActiveSupport::Duration.new(num * 365.25.days.to_i, [[:years, num]])
   end
   alias :gentlemans_year :gentlemans_years
 end
--- lib/gentlemans_years/version.rb
+++ lib/gentlemans_years/version.rb
@@ -1,3 +1,3 @@
 module GentlemansYears
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
